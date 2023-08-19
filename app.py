fdf8942ca04fc92108ed7183b31c16096e9d39d1
SN4k <110192750+SN4kNARWADE@users.noreply.github.com>
2023-08-19 22:54:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from bardapi import Bard
 from flask import Flask, request,jsonify,json
 from flask_restful import Resource, Api
 
-app = Flask(_name_)
+app = Flask(__name__)
 api = Api(app)
 
 
@@ -24,7 +24,7 @@ def search():
         prompt = args.get("question", default="", type=str)
         prompt = "warning!! only answer the question which is requested and do not give any other texts in the response! i am giving you this json with keys as some questions and the values as answer to the respective question, i will tell you what is the question and you will answer me by finding that question in the json as key and tell me the answer according to the value of that key, the json is {'hi' :'hello','how are you?':'i am good','hello':'hii'} and the question is '{question}' , wrap the answer in ]"
 
-        bard = Bard(token="aAjW3souphQ7tT0Rf8rviWsBBOH30ZuLtHVK3at4V-O9-K2IraoxcP_mEq6JG15KgK0EHA.")
+        bard = Bard(token=token)
 
         result = bard.get_answer(prompt)
 
@@ -37,6 +37,6 @@ def search():
 
 print(search())
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     app.run(debug=False)
     
